bd032f514e5416c6d8d139069ab4f65bc1add772
Carina Cruz <lecruz@bu.edu>
2018-05-30 00:59:25 +0000

    in progress map the debris

--- map_the_debris.js
+++ map_the_debris.js
@@ -5,4 +5,29 @@ function orbitalPeriod(arr) {
   return arr;
 }
 
+var newArray= []; //Create a new array for the name,orbital
+
+var T = 2 * math.PI; //multiply pi by 2
+
+var determineOrbital = function(object) {
+//   var U = Radius**AvgAlt; //radius and altitude to the 3rd power
+var U = math.pow(radius + AvgAlt * 3);
+var V = U/GM;  //(r+AA/GM of Earth)
+
+ //use math.round as the last step
+ //delete satelite
+};
+
+
+//Getting the orbital
+//multiply pi by 2
+//Get the sqrt of r + AA cubed divided by GM
+
+
+var sqrt = math.sqrt(6367+AA);
+
+
+
+
+
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
